a591107666e6cf450cb7e1c872a1583743639560
fathimaca369 <fathimacafathimaca152@gmail.com>
2025-11-27 09:31:59 +0400

    Add files via upload

---
+++ co5/pgm1.py
@@ -0,0 +1,2 @@
+A teacher is a professional who guides students to acquire knowledge and develop skills, acting as a crucial mentor and role model
+. Beyond academics, teachers foster critical thinking, instill values, and help shape a student's character, making them a foundational part of a student's life and future. Their dedication inspires students to overcome challenges and strive for their best.
